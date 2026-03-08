c37ff1d43f8d40bb5d9d5161a2904201da4bd22b
Abra-oMaker <chicagameplayofficial123@gmail.com>
2026-03-08 22:02:32 +0000

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,7 +26,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
+    Component.PageTitle({ pageUrl: "Bem vindo" }),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
@@ -41,7 +41,15 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        depth: 2,
+        showTags: false,
+      },
+      globalGraph: {
+        showTags: false,
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
@@ -51,7 +59,7 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
-    Component.PageTitle(),
+    Component.PageTitle({ pageUrl: "Bem vindo" }),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
