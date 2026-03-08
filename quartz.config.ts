e32ac0726dcdc53eea3d1f46693bd1d7aa657a88
Abra-oMaker <chicagameplayofficial123@gmail.com>
2026-03-08 21:55:09 +0000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,6 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "📔 GugaViaro's Vault",
+    homePage: "Bem vindo",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
